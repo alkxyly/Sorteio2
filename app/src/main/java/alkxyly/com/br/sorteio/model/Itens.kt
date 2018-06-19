e87ebb7dbbadba9e02ef2e47a5020d1308efc950
Alkxyly <alkxyly@hotmail.com>
2018-06-19 08:27:39 -0300

    Adicionando Atributos do item da tabela

--- app/src/main/java/alkxyly/com/br/sorteio/model/Itens.kt
+++ app/src/main/java/alkxyly/com/br/sorteio/model/Itens.kt
@@ -4,4 +4,12 @@ import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
-data class  Itens(var identificador: Int, var nome: String, var flag: Boolean): Parcelable
+data class  Itens(var identificador: Int, var nome: String,
+                  var rg: String,
+                  var cpf: String,
+                  var valor: String,
+                  var celular: String,
+                  var endereco: String,
+                  var dataVenda: String,
+                  var dataRecebimento: String,
+                  var pago: Boolean): Parcelable
